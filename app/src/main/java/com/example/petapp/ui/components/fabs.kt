ed2ddc06ddc47fa6c69b82261544e12713791978
Jakub Nowak <jakub.nowak.eu@gmail.com>
2023-07-26 13:32:24 +0200

    Drawable resources commit

--- app/src/main/java/com/example/petapp/ui/components/fabs.kt
+++ app/src/main/java/com/example/petapp/ui/components/fabs.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 
--- app/src/main/java/com/example/petapp/ui/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/petapp/ui/dashboard/DashboardScreen.kt
@@ -1,18 +1,24 @@
 package com.example.petapp.ui.dashboard
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.petapp.ui.components.*
+import com.example.petapp.ui.components.AddSimpleFab
+import com.example.petapp.ui.components.ErrorScreen
+import com.example.petapp.ui.components.LoadingScreen
+import com.example.petapp.ui.components.NoContentPrev
+import com.example.petapp.ui.components.PetItem
 import com.example.petapp.ui.navigation.DefaultScaffold
 
 @Composable
